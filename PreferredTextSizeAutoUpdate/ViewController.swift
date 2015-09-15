2bebbc15f3b71b300b39fea235b526a76eb88efd
John Regner <john@johnregner.com>
2015-09-14 19:49:44 -0700

    Added DynamicBodyTextView so that view logic is encapsulated into a view class and not cluttering up our view controller.

--- PreferredTextSizeAutoUpdate/ViewController.swift
+++ PreferredTextSizeAutoUpdate/ViewController.swift
@@ -2,16 +2,4 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textView: UITextView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        textView.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
-        let notificationCenter = NSNotificationCenter.defaultCenter()
-        notificationCenter.addObserver(self, selector: "textSizeWasUpdated:", name: UIContentSizeCategoryDidChangeNotification, object: nil)
-    }
-
-    func textSizeWasUpdated(note: NSNotification){
-        textView.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
-    }
 }
